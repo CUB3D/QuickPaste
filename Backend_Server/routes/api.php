04269574e24cbeede99e1a1583dd625514be8adb
CUB3D <callumthom11@gmail.com>
2019-02-03 14:42:14 +0000

    MVP done

--- Backend_Server/routes/api.php
+++ Backend_Server/routes/api.php
@@ -13,13 +13,59 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::middleware('guest:api')->get("/newNote", function(Request $request) {
 
+    // Generate two strings, one the public note id, one a private token for edit perms
+    $noteKey = substr(str_shuffle(MD5(microtime())), 0, 5);
+    $noteToken = substr(str_shuffle(MD5(microtime())), 0, 15);
 
+    // Store the key and the token for auth later
+    DB::insert("INSERT INTO Notes (noteKey, noteToken) VALUES (?, ?)",
+                [$noteKey, $noteToken]);
 
-    return "Hello";
+    // Return values
+    return [
+        "NoteKey" => $noteKey,
+        "NoteToken" => $noteToken
+    ];
+});
+
+Route::middleware("guest:api")->get("/readNote", function(Request $request) {
+   $key = $request->get("NoteKey");
+
+   if(preg_match("/[A-Za-z0-9]+/", $key)
+      && Storage::exists("notes/$key.txt")) {
+
+       $content = Storage::get("notes/$key.txt");
+
+       return [
+           "Status" => 0,
+           "Name" => "ASFD",
+           "Content" => $content
+       ];
+   }
+
+   return [
+       "Status" => 1,
+       "Error" => "Invalid key"
+   ];
+});
+
+Route::middleware("guest:api")->post("/saveNote", function(Request $request) {
+    $json = $request->json()->all();
+    $noteKey = $json["NoteKey"];
+    $noteToken = $json["NoteToken"];
+    $content = $json["Content"];
+
+    Storage::put("notes/$noteKey.txt", $content);
+
+    //TODO: check that the params are valid, nk exists and nt is its token
+
+//    return [
+//        "Name" => "TestNote",
+//        "Age" => 0,
+//        "Content" => "ASDFSADF\nASDFASDF\nASDFASDF"
+//    ];
+
+    return "";
 });
--- src/App.js
+++ src/App.js
@@ -2,14 +2,99 @@ import React, { Component } from 'react';
 import './App.css';
 import {Switch, Route, withRouter} from 'react-router-dom'
 import logo from './logo.svg'
+import $ from 'jquery'
+import Cookies from 'universal-cookie'
+
+// Used to store the note token over routes and reloads
+const cookies = new Cookies();
+
 
 class Editor extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            TextContent: "",
+            InitialContent: "",
+            NoteKey: cookies.get("NoteKey"),
+            NoteToken: cookies.get("NoteToken"),
+            dirty: false
+        };
+
+        // Check to see if reloading the editor
+        $.ajax({
+            url: "http://localhost:8000/api/readNote",
+            type: "GET",
+            crossDomain: true,
+            crossOrigin: true,
+            dataType: "json",
+            data: "NoteKey=" + this.state.NoteKey,
+            success: function (e) {
+
+                if(e.Status === 0) {
+                    let content = new Buffer(e.Content, 'base64').toString("utf-8");
+                    this.setState({"InitialContent": content});
+                }
+
+                console.log(e)
+            }.bind(this)
+        });
+    }
+
+    handleTextChange = (e) => {
+        this.setState({
+            TextContent: e.target.innerHTML,
+            dirty: true
+        }, function() {
+            $.ajax({
+                url: "http://localhost:8000/api/saveNote",
+                type: "POST",
+                crossDomain: true,
+                crossOrigin: true,
+                dataType: "json",
+                data: JSON.stringify({
+                    NoteKey: this.state.NoteKey,
+                    NoteToken: this.state.NoteToken,
+                    Content: new Buffer(this.state.TextContent).toString("base64")
+                })
+            })
+        }.bind(this));
+    };
+
+    onKeyPress = (e) => {
+        let char = String.fromCharCode(e.which).toLowerCase();
+
+        if(e.ctrlKey && char === "s") {
+            e.preventDefault();
+
+            this.setState({
+                dirty: false
+            });
+
+            $.ajax({
+                url: "http://localhost:8000/api/saveNote",
+                type: "POST",
+                crossDomain: true,
+                crossOrigin: true,
+                dataType: "json",
+                data: JSON.stringify({
+                    NoteKey: this.state.NoteKey,
+                    NoteToken: this.state.NoteToken,
+                    Content: new Buffer(this.state.TextContent).toString("base64")
+
+                })
+            });
+        }
+    };
+
     render() {
         return (
-            <div className="Edit">
-                <textarea className="Edit-text">
-
-                </textarea>
+            <div className="Edit-Holder">
+                <div className="Edit-Status">
+                    <p className="Edit-Status-Element">/note/{this.state.NoteKey}{this.state.dirty? "*" : ""}</p>
+                </div>
+            <div className="Edit Edit-text" contentEditable={true} onInput={this.handleTextChange} onKeyDown={this.onKeyPress} dangerouslySetInnerHTML={{__html: this.state.InitialContent}}>
+            </div>
             </div>
         );
     }
@@ -17,7 +102,26 @@ class Editor extends Component {
 
 class Menu extends Component {
     onClickNewNote = () => {
-        this.props.history.push("/note/" + Math.random().toString(36).substring(7))
+        // Tell server to generate a new note
+        $.ajax({
+            url: "http://localhost:8000/api/newNote",
+            type: "GET",
+            crossDomain: true,
+            crossOrigin: true,
+            success: function(e) {
+                this.setState({
+                    NoteKey: e.NoteKey,
+                    NoteToken: e.NoteToken
+                });
+
+                cookies.set("NoteKey", e.NoteKey);
+                cookies.set("NoteToken", e.NoteToken);
+
+                this.props.history.push("/edit/" + e.NoteKey);
+            }.bind(this)
+        });
+
+        //this.props.history.push("/edit")
     };
 
     render() {
@@ -39,7 +143,7 @@ class App extends Component {
       return (
           <Switch>
               <Route path="/" exact component={Menu}/>
-              <Route path="/note" component={Editor}/>
+              <Route path="/edit" component={Editor}/>
           </Switch>
       );
   }
